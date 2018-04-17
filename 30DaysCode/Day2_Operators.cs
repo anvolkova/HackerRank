f0df748685dea7a34f8c4559507aca132e7286e1
Anna Volkova <annettavo@gmail.com>
2018-04-17 21:15:57 +1000

    Day 2

---
+++ 30DaysCode/Day2_Operators.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+        double tip = meal_cost * tip_percent/100;
+        double tax = meal_cost * tax_percent/100;
+        double totalCost = meal_cost + tip + tax;
+        Console.WriteLine("The total meal cost is " + Math.Round(totalCost) + " dollars.");
+    }
+}
